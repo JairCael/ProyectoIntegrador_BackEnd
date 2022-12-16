4cd80623ca8e921b39c623691742e4a7c5641bfc
JairCael <jaircayahuaelescano@gmail.com>
2022-12-16 13:36:57 -0500

    Prueba_01

--- database/db.js
+++ database/db.js
@@ -1,9 +1,11 @@
 import Sequelize from 'sequelize';
 
-export const sequelize = new Sequelize('asistencia', 'root', 'mysql', {
+/* export const sequelize = new Sequelize('asistencia', 'root', 'mysql', {
     host: 'localhost',
     dialect: 'mysql',
     port: '3308'
-})
+}) */
 
 /* export const sequelize = new Sequelize('mysql://root:KdVKFhRUVC9fekeolnkl@containers-us-west-37.railway.app:5469/railway') */
+export const sequelize = new Sequelize(`mysql://root:${process.env.DBROOT}@containers-us-west-165.railway.app:5698/railway`)
+
